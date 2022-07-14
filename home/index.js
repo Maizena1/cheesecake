e3527fb5bf2ab235149d1a6a1e2ba54d1c3be729
Luis Antes <edy.1305@hotmail.com>
2022-07-14 16:24:13 -0600

    Finish fragment to dots elements and update fragment to heroTitle elements

--- home/index.js
+++ home/index.js
@@ -1,9 +1,32 @@
+const heroTitle = document.getElementById("hero-title");
+const highlightsDots = document.getElementById("highlights-dots");
+const loadData = async () => (await fetch("../data-hero.json")).json();
+const heroFragment = document.createDocumentFragment();
+const dotsFragment = document.createDocumentFragment();
 
-const loadData = async () => (await fetch("../data-hero.json")).json()
 
 async function init(){
-    const data = loadData()
-    console.log(data)
+    const data = await loadData();
+    data.forEach((item) => {
+
+        const lengthDataArray = data.length;
+
+        if (item.title){
+            const h1 = document.createElement("h1");
+            h1.textContent = item.title;
+            heroFragment.appendChild(h1);
+        }
+
+        const dot = document.createElement("button");
+        dot.classList.add("dot")
+        dotsFragment.appendChild(dot);
+    });
+    
+    heroTitle.appendChild(heroFragment);
+    highlightsDots.appendChild(dotsFragment);
 }
+
+
+
 
 init()
