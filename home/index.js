666b5c7f9c448240365a4176baf186da760af755
Luis Antes <edy.1305@hotmail.com>
2022-07-16 21:39:23 -0600

    add booking page

--- home/index.js
+++ home/index.js
@@ -1,32 +1,53 @@
-const heroTitle = document.getElementById("hero-title");
-const highlightsDots = document.getElementById("highlights-dots");
-const loadData = async () => (await fetch("../data-hero.json")).json();
-const heroFragment = document.createDocumentFragment();
-const dotsFragment = document.createDocumentFragment();
 
+const dotsFragment = document.createDocumentFragment();
+const Highlights = document.getElementById("highlights");
+const HighlightsDots = document.getElementById("highlights-dots");
+
+let currentHeroIndex = 0;
+
+const loadData = async () => (await fetch("../data-hero.json")).json();
 
 async function init(){
     const data = await loadData();
-    data.forEach((item) => {
+    data.forEach((item, index) => {
 
-        const lengthDataArray = data.length;
-
-        if (item.title){
-            const h1 = document.createElement("h1");
-            h1.textContent = item.title;
-            heroFragment.appendChild(h1);
-        }
 
         const dot = document.createElement("button");
         dot.classList.add("dot")
         dotsFragment.appendChild(dot);
+
+        dot.addEventListener("click", () => {
+
+            const h1 = document.createElement("h1");
+            const h2 = document.createElement("h2");
+
+            const heroFragment = document.createDocumentFragment();
+
+            Highlights.innerHTML = "";
+ 
+            h1.textContent = item.title;
+            h2.textContent = item.subtitle;
+
+            heroFragment.appendChild(h1);
+            heroFragment.appendChild(h2);
+
+            h1.classList.add("title")
+
+            if(item.button){
+                
+                const button = document.createElement("button");
+                button.textContent = item.button;
+                heroFragment.appendChild(button);
+                button.classList.add("highlights-button");
+
+            }
+            
+            Highlights.appendChild(heroFragment);
+        })
+
     });
     
-    heroTitle.appendChild(heroFragment);
-    highlightsDots.appendChild(dotsFragment);
+    HighlightsDots.appendChild(dotsFragment);
 }
-
-
-
 
 init()
