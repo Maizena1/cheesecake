18fa9dc8b726e9243d314863f2fb76b66ef952fd
Antes <ciul060@gmail.com>
2022-08-17 14:03:14 -0600

    Update Mas button

--- booking/booking-MasButton.js
+++ booking/booking-MasButton.js
@@ -1,6 +1,19 @@
+
+
 const masButton = document.getElementById("mas-button")
 const bookingContentContainer = document.getElementById("booking-content-container")
 
+let estado = true;
+
 masButton.addEventListener(("click"), () => {
-    bookingContentContainer.classList.add("booking-content-container-function")
+    
+    if (estado) {
+        bookingContentContainer.classList.add("booking-content-container-function-on")
+        bookingContentContainer.classList.remove("booking-content-container-function-off")
+        estado = false;
+        
+    }else{
+        bookingContentContainer.classList.add("booking-content-container-function-off")
+        estado = true ;
+    }
 })
